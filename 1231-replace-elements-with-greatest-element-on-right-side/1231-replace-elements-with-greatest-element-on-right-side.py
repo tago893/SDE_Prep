c0d784c951f9f86482e64228ec8b2f13b17e6126
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-07-18 23:34:22 -0700

    Time: 46 ms (31.04%), Space: 19.2 MB (13.18%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
@@ -2,8 +2,8 @@ class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         n = len(arr)
         result = [0]*(n)
-        prefix = -1
-        result[n-1] = prefix
+        suffix = -1
+        result[n-1] = suffix
         for i in range(n-2,-1,-1):
             result[i] =  max(result[i+1],arr[i+1])
         
