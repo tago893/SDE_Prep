9129c2402da5b4df8c34fbd237a55a020a7065d0
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-07-30 15:49:25 -0700

    Time: 3 ms (42.53%), Space: 17.9 MB (73.62%) - LeetHub

--- 0983-validate-stack-sequences/0983-validate-stack-sequences.py
+++ 0983-validate-stack-sequences/0983-validate-stack-sequences.py
@@ -6,16 +6,9 @@ class Solution:
         n = len(pushed)
 
         for i in range(0,n):
-            while vstack and vstack[-1] == popped[j]:
+            vstack.append(pushed[i])
+            while vstack and j< n and vstack[-1] == popped[j]:
                 vstack.pop()
                 j+=1
-            vstack.append(pushed[i])
-
-        while j<n:
-            if vstack[-1] != popped[j]:
-                break
-            else:
-                vstack.pop()
-            j+=1
-        
+       
         return len(vstack) == 0
