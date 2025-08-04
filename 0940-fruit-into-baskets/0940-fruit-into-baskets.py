a68fbf22297ae7119d096ce7a8f00b9a9d1d0d07
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-08-03 18:48:25 -0700

    Time: 195 ms (31.93%), Space: 23.8 MB (16.47%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        i,j = 0,0
+        n = len(fruits)
+        count = defaultdict(int)
+        max_number = 0
+
+        while j<n:
+            count[fruits[j]]+=1
+            if len(count)>=3:
+                while len(count)>=3:
+                    count[fruits[i]]-=1
+                    if count[fruits[i]] == 0:
+                        del count[fruits[i]]
+                    i+=1
+            
+            max_number = max(max_number,j-i+1)
+            j+=1
+        
+        return max_number
+
+
+        
