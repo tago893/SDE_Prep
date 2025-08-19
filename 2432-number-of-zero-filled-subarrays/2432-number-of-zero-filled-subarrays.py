cfe0c286ea7f5b79821713df38dba58ab143edb7
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-08-19 05:04:34 -0700

    Time: 43 ms (41.07%), Space: 28.4 MB (83.4%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        count_zero = 0
+        ans = 0
+        n = len(nums)
+        for i in range(0,n):
+            if nums[i] == 0:
+                count_zero += 1
+                ans += count_zero
+            else:
+                count_zero=0
+        return ans
