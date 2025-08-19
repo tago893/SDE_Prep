9eddea1747797bae5558c8a23910e50b96d77410
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-08-19 05:14:21 -0700

    Time: 40 ms (45.44%), Space: 28.6 MB (13.59%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.py
@@ -7,7 +7,6 @@ class Solution:
             if nums[i] == 0:
                 count_zero+=1 
             else:
-                print([count_zero,i])
                 ans += count_zero*(count_zero+1)//2
                 count_zero=0
         ans+=count_zero*(count_zero+1)//2
