4176d367961d45df726ac07ab2cc2c25f2ac10c5
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-08-25 13:52:16 -0700

    Time: 183 ms (41.85%), Space: 39.7 MB (30.98%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,18 +1,9 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        cars_ds = []
-        n = len(position)
-        for i in range(0,n):
-            cars_ds.append((position[i],speed[i]))
-        # sort the groups based on position
-        cars_ds.sort(key = lambda x:x[0],reverse=True)
-
-        fleets = 1
-        prevTime = (target - cars_ds[0][0]) / cars_ds[0][1]
-        for i in range(1,len(cars_ds)):
-            currCar = cars_ds[i]
-            currTime = (target - currCar[0]) / currCar[1]
-            if currTime > prevTime:
-                fleets+=1
-                prevTime = currTime
-        return fleets
+        time = [float(target - p) / s for p, s in sorted(zip(position, speed))]
+        res = cur = 0
+        for t in time[::-1]:
+            if t > cur:
+                res += 1
+                cur = t
+        return res
