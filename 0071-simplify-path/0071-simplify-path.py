1a15f6842e94af9b7c93d53a3112c701552b2fef
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-09-06 00:13:55 -0700

    Time: 58 ms (5.89%), Space: 18.2 MB (13.19%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -11,10 +11,6 @@ class Solution:
             elif c!="." and c!=".." and c!="":
                 stack.append(c)
         
-        if len(stack) == 0:
-            new_path="/"
-        for c in stack:
-            if c!="." and c!="":
-                new_path+= "/"+c 
+        
             
-        return (new_path)
+        return "/" + "/".join(stack)
