a15150635c995127e15a7dc9bd14d00566097b11
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-09-08 19:17:52 -0700

    Time: 39 ms (44.58%), Space: 18 MB (26.61%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.py
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -6,12 +6,13 @@ class Solution:
         left = 1
         right = n
 
-        while left<=right:
+        while left<right:
             mid = left + (right-left)//2
             if isBadVersion(mid) == False:
                 left = mid+1
             else:
-                right = mid-1
+                right = mid
+                
                 
 
         return(left)            
