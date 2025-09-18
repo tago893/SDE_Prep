0853ac8f753273a9e76ed005faff0cdb24a148ac
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-09-18 16:08:43 -0700

    Time: 3 ms (71.11%), Space: 18.6 MB (38.2%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -20,8 +20,9 @@ class Solution:
         
         for i in range(1,m):
             for j in range(1,n):
-                if matrix[i][0]== 0 or matrix[0][j] == 0:
-                    matrix[i][j] = 0
+                if matrix[i][j]!=0:
+                    if matrix[i][0]== 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
         
         if first_row:
             for j in range(0,n):
