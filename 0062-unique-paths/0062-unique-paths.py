53c174ae5d6693e1a439c32a3d368e7b3802ed6c
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-09-19 16:41:47 -0700

    Time: 0 ms (100%), Space: 17.8 MB (45.82%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,21 +1,19 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         def solve(i,j,t):
-            #Valid path
             if i==m-1 and j==n-1:
                 return 1
-            #Not valid path
-            if i<0 or j>=n or i>=m:
+            if i>=m or j>=n:
                 return 0
             
             if t[i][j] != -1:
                 return t[i][j]
+            
             right = solve(i,j+1,t)
             down = solve(i+1,j,t)
-
-            t[i][j]=right+down
-            return t[i][j] 
+            t[i][j] = right + down
+            return t[i][j]
         
-        t=[[-1 for _ in range(n+1)]for _ in range(m+1)]
+        t = [[-1 for _ in range(n)]for _ in range(m)]
         return solve(0,0,t)
         
