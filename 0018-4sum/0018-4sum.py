1ac204ce4ff37795599eb38e1c114a6a51834f94
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-09-20 17:39:01 -0700

    Time: 632 ms (12.62%), Space: 17.8 MB (83.13%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -2,28 +2,30 @@ class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         nums.sort()
         n = len(nums)
-        res = []
-
+        result = []
         for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
+            if i>0 and nums[i] == nums[i-1]:
                 continue
-            for j in range(i + 1, n):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+            for j in range(i+1,n):
+                if j>i+1 and nums[j] == nums[j-1]:
                     continue
-                left, right = j + 1, n - 1
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-                    if total == target:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-                        while left < right and nums[right] == nums[right + 1]:
-                            right -= 1
-                    elif total < target:
-                        left += 1
+                l = j+1
+                r = n-1
+                while l<r:
+                    if nums[i]+nums[j]+nums[l]+nums[r] == target:
+                        result.append([nums[i],nums[j],nums[l],nums[r]]) 
+                        l+=1
+                        r-=1
+                        while l<r and nums[l]==nums[l-1]:
+                            l+=1
+                        while l<r and nums[r]==nums[r+1]:
+                            r-=1
+                        
+                            
+                    elif nums[i]+nums[j]+nums[l]+nums[r] > target:
+                        r-=1
                     else:
-                        right -= 1
+                        l+=1
+                
 
-        return res
+        return result
