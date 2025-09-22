4f57ed6da9ac7ef729537d09856ca1f0a5e178e9
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-09-22 12:19:31 -0700

    Time: 0 ms (100%), Space: 18 MB (40.12%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
         n = len(nums)
-        def solve(i,curr_list):
-            if i >= n:
-                result.append(curr_list[:])
-                return 
-            curr_list.append(nums[i])
-            solve(i+1,curr_list)
-            curr_list.pop()
-            solve(i+1,curr_list)
+        result = []
+        def solve(i,combination):
+            nonlocal result
+            if i>=n:
+                result.append(combination[:])
+                return
+            #take
+            combination.append(nums[i])
+            solve(i+1,combination)
+            #not take
+            combination.pop()
+            solve(i+1,combination)
+            
+
         solve(0,[])
         return result
