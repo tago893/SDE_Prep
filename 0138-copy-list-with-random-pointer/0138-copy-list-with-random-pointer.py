a178593ddc13ec59f63d6f3c845668913e51b868
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-10-17 12:58:04 -0700

    Time: 43 ms (45.12%), Space: 18.7 MB (19.19%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -12,43 +12,22 @@ class Solution:
         if not head:
             return None
 
-        # Dummy node
-        x = Node(0)
-        temp2 = x
+        old_to_new = {}
         temp = head
-        i = 0
 
-        # hash_map: index -> original.random node
-        # new_list_map: index -> new node
-        hash_map = {}
-        new_list_map = {}
-
-        # \U0001fa79 First pass: copy nodes & next pointers
+        # 1️⃣ Create all nodes (just values)
         while temp:
-            new_node = Node(temp.val)
-            temp2.next = new_node
-            hash_map[i] = temp.random     # store random target (original reference)
-            new_list_map[i] = new_node    # store new node
-            temp2 = temp2.next
+            old_to_new[temp] = Node(temp.val)
             temp = temp.next
-            i += 1
 
-        # \U0001f9ed Second pass: assign random pointers
-        # We'll find which index each original node corresponds to.
-        # To do this, map old node -> index
-        old_to_index = {}
+        # 2️⃣ Link next and random
         temp = head
-        j = 0
         while temp:
-            old_to_index[temp] = j
+            if temp.next:
+                old_to_new[temp].next = old_to_new[temp.next]
+            if temp.random:
+                old_to_new[temp].random = old_to_new[temp.random]
             temp = temp.next
-            j += 1
 
-        # Now go through each index and assign randoms in the new list
-        for idx in range(i):
-            original_random = hash_map[idx]  # old random node
-            if original_random is not None:
-                random_idx = old_to_index[original_random]
-                new_list_map[idx].random = new_list_map[random_idx]
+        return old_to_new[head]
 
-        return x.next
