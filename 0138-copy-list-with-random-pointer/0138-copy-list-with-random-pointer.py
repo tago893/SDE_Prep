ce028455a609ed49b20d5e7c2efd6d240ce0c57e
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-10-17 15:39:34 -0700

    Time: 40 ms (66.44%), Space: 18.5 MB (59.89%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -7,41 +7,20 @@ class Node:
         self.random = random
 """
 
-
-
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        if not head:
+        if head == None:
             return None
-
-        dummy = Node(0)
-        temp2 = dummy
-        temp = head
-
-        # Maps
-        old_to_index = {}
-        index_to_random = {}
-        index_to_newnode = {}
-
-        i = 0
-        # \U0001f539 First pass: copy list & record mappings
-        while temp:
-            new_node = Node(temp.val)
-            temp2.next = new_node
-
-            old_to_index[temp] = i
-            index_to_random[i] = temp.random
-            index_to_newnode[i] = new_node
-
-            temp2 = new_node
-            temp = temp.next
-            i += 1
-
-        # \U0001f539 Second pass: assign random pointers
-        for idx, old_random_node in index_to_random.items():
-            if old_random_node:
-                random_idx = old_to_index[old_random_node]
-                index_to_newnode[idx].random = index_to_newnode[random_idx]
-
-        return dummy.next
-
+        old_to_new  = {}
+        c = head
+        while c:
+            old_to_new[c] = Node(c.val)
+            c = c.next
+        
+        c = head
+        while c:
+            old_to_new[c].next = old_to_new.get(c.next)
+            old_to_new[c].random = old_to_new.get(c.random)
+            c = c.next
+        
+        return old_to_new[head]
