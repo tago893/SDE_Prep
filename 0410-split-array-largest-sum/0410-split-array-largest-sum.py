b8376a976d33a63b2109cbeb0739923adbf28fd4
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-11-04 16:13:06 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,29 +1,34 @@
 class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
+        sum_of_the_arrays = []
+        def subarrays(nums):
+            for i in range(0,len(nums)):
+                sums=0
+                for j in range(i,len(nums)):
+                    sums+=nums[j]
+                sum_of_the_arrays.append(sums)
+            return sum_of_the_arrays
         
+        subarrays(nums)
+        l,r = min(sum_of_the_arrays),max(sum_of_the_arrays)
         
         def check(sum_value):
-            total = 0
             K=1
+            total = 0
             for num in nums:
                 total+=num
                 if total>sum_value:
-                    total = num
                     K+=1
+                    total=num
                     if K>k:
                         return False
+                
             return True
-            
-        l,r = max(nums),sum(nums)
         while l<r:
             mid = l+(r-l)//2
             if check(mid):
                 r = mid
             else:
-                l=mid+1 
-
-        
+                l = mid+1
         return l
-        
-        
                 
