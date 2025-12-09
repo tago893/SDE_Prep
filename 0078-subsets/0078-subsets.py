b52fd71a104a92315b2c7cba87c47a28c761a4a3
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-12-08 16:11:57 -0800

    Time: 0 ms (100%), Space: 17.9 MB (64.52%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -3,17 +3,10 @@ class Solution:
         n = len(nums)
         result = []
         def solve(i,combination):
-            nonlocal result
-            if i>=n:
-                result.append(combination[:])
-                return
-            #take
-            combination.append(nums[i])
-            solve(i+1,combination)
-            #not take
-            combination.pop()
-            solve(i+1,combination)
-            
-
+            result.append(combination[:])
+            for k in range(i,n):
+                combination.append(nums[k])
+                solve(k+1,combination)
+                combination.pop()
         solve(0,[])
         return result
