b2625bc1db16424512992cdfd3214d510b23319a
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-12-17 14:13:48 -0800

    Time: 0 ms (100%), Space: 17.9 MB (10.13%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0 or x == 1:
+            return x
+        l, r = 0, x
+        while l <= r:
+            mid = l + (r - l) // 2
+            square = mid * mid
+            if square == x:
+                return mid
+            elif square > x:
+                r = mid - 1
+            else:
+                l = mid + 1
+                ans = mid  # mid is a valid floor sqrt so far
+        return ans
