440dcceb3c0e5e6db26b90f99f307d5295027d04
VARUN CHIKKALA <63788998+tago893@users.noreply.github.com>
2025-12-18 15:40:12 -0800

    Time: 515 ms (5.11%), Space: 32.7 MB (37.47%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,28 +1,34 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        n = len(heights)
-        stack = []
-
-        leftMost = [-1] * n
-        for i in range(n):
-            while stack and heights[stack[-1]] >= heights[i]:
-                stack.pop()
-            if stack:
-                leftMost[i] = stack[-1]
-            stack.append(i)
-
-        stack = []
-        rightMost = [n] * n
-        for i in range(n - 1, -1, -1):
-            while stack and heights[stack[-1]] >= heights[i]:
-                stack.pop()
-            if stack:
-                rightMost[i] = stack[-1]
-            stack.append(i)
-
+        n  = len(heights)
         maxArea = 0
-        for i in range(n):
-            leftMost[i] += 1
-            rightMost[i] -= 1
-            maxArea = max(maxArea, heights[i] * (rightMost[i] - leftMost[i] + 1))
+
+        stack = []
+        leftmost = [-1]*(n)
+
+        for i in range(0,n):
+            while stack and heights[stack[-1]]>= heights[i]:
+                stack.pop()
+            if stack:
+                leftmost[i] = stack[-1]
+            stack.append(i)
+        
+        stack = []
+        rightmost = [n]*n
+        for i in range(n-1,-1,-1):
+            while stack and heights[stack[-1]]>= heights[i]:
+                stack.pop()
+            if stack:
+                rightmost[i] = stack[-1]
+            stack.append(i)
+        
+        print(leftmost)
+        print(rightmost)
+
+        for i in range(0,n):
+            leftmost[i]+=1
+            rightmost[i]-=1
+            maxArea = max(maxArea,heights[i],heights[i]*(rightmost[i]-leftmost[i]+1))
+            print(maxArea)
         return maxArea
+        
